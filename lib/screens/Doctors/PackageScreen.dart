e434543f4a8617fea5d508fbbe4bc52ae648a954
Maria Soffie <mariasoffie.17@gmail.com>
2024-04-25 20:58:27 +0800

    update package screen

--- lib/screens/Doctors/PackageScreen.dart
+++ lib/screens/Doctors/PackageScreen.dart
@@ -3,24 +3,40 @@ import 'package:flutter/material.dart';
 class PackageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextButton(
-              style: TextButton.styleFrom(
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              onPressed: null,
-              child: const Text('Add'),
-            ),
-            const SizedBox(height: 30),
-            Text('Package 1'),
-            Text('package 2')
-          ],
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          width: double.infinity,
+          height: 200,
         ),
-      ),
+        Positioned(
+          top: 20,
+          left: 20,
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text(
+              'Package 1',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Package 1',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            )
+          ]),
+        ),
+      ]),
     );
   }
 }
