8f750bb6b11ba252ed088742ace2f53ff4278faf
Maria Soffie <mariasoffie.17@gmail.com>
2024-04-30 18:14:29 +0800

    update consultation patient screen

--- lib/screens/Patients/ConsultationScreen.dart
+++ lib/screens/Patients/ConsultationScreen.dart
@@ -49,6 +49,7 @@ class ConsultationState extends State<Consultation> {
               'Select Doctor',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+            DropDownSelectDoctor(),
             SizedBox(height: 10),
             Text(
               'Select Date',
@@ -77,9 +78,111 @@ class ConsultationState extends State<Consultation> {
               'Select Consultation Type',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+            DropDownSelectType(),
             SizedBox(height: 20),
             MakeAppointmentButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class DropDownSelectDoctor extends StatefulWidget {
+  @override
+  DropDownDoctor createState() => DropDownDoctor();
+}
+
+class DropDownDoctor extends State<DropDownSelectDoctor> {
+  String _selectedItem = 'Pediatric'; // Initial selected item
+
+  // List of dropdown items
+  List<String> _dropdownItems = [
+    'dr. Jake Sim - Pediatric',
+    'dr. Heeseung Lee - Dentist',
+    'dr. Jay Park - General Practitioner',
+    'dr. Sunghoon Park - Orthopedic Surgeon',
+    'dr. Jungwon Yang - Dermatologist',
+    'dr. Sunoo Park - Obstetrician',
+    'dr. Niki - Cardiologist',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Specialist'),
+      ),
+      body: Center(
+        child: DropdownButton<String>(
+          value: _selectedItem,
+          icon: Icon(Icons.arrow_downward),
+          iconSize: 24,
+          elevation: 16,
+          style: TextStyle(color: Colors.deepPurple),
+          underline: Container(
+            height: 2,
+            color: Colors.deepPurpleAccent,
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              _selectedItem = newValue!;
+            });
+          },
+          items: _dropdownItems.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class DropDownSelectType extends StatefulWidget {
+  @override
+  DropDownType createState() => DropDownType();
+}
+
+class DropDownType extends State<DropDownSelectType> {
+  String _selectedItem = 'Pediatric'; // Initial selected item
+
+  // List of dropdown items
+  List<String> _dropdownItems = [
+    'Offline Consultation',
+    'Online Consultation',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Specialist'),
+      ),
+      body: Center(
+        child: DropdownButton<String>(
+          value: _selectedItem,
+          icon: Icon(Icons.arrow_downward),
+          iconSize: 24,
+          elevation: 16,
+          style: TextStyle(color: Colors.deepPurple),
+          underline: Container(
+            height: 2,
+            color: Colors.deepPurpleAccent,
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              _selectedItem = newValue!;
+            });
+          },
+          items: _dropdownItems.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
         ),
       ),
     );
