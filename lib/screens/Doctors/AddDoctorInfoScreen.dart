0bb7aa809825400f967fcbd44d1690c1559a8344
Maria Soffie <mariasoffie.17@gmail.com>
2024-05-04 22:19:31 +0800

    update add doctor information screen

--- lib/screens/Doctors/AddDoctorInfoScreen.dart
+++ lib/screens/Doctors/AddDoctorInfoScreen.dart
@@ -1,150 +1,17 @@
 import 'package:flutter/material.dart';
 
-class AddDoctorInformation extends StatelessWidget {
-  bool isChecked = false;
+class AddDoctorInformation extends StatefulWidget {
+  const AddDoctorInformation({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: 20),
-            Text(
-              'Select Specialist',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            DropDownSelectSpecialist(),
-            SizedBox(height: 10),
-            Text(
-              'Work Day',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Checkbox(
-            //       value: isChecked,
-            //       onChanged: (bool? value) {
-            //         setState(() {
-            //           isChecked = value!;
-            //         });
-            //       },
-            //     ),
-            //     Text('Monday'),
-            //   ],
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Checkbox(
-            //       value: isChecked,
-            //       onChanged: (bool? value) {
-            //         setState(() {
-            //           isChecked = value!;
-            //         });
-            //       },
-            //     ),
-            //     Text('Tuesday'),
-            //   ],
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Checkbox(
-            //       value: isChecked,
-            //       onChanged: (bool? value) {
-            //         setState(() {
-            //           isChecked = value!;
-            //         });
-            //       },
-            //     ),
-            //     Text('Wednesday'),
-            //   ],
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Checkbox(
-            //       value: isChecked,
-            //       onChanged: (bool? value) {
-            //         setState(() {
-            //           isChecked = value!;
-            //         });
-            //       },
-            //     ),
-            //     Text('Thursday'),
-            //   ],
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Checkbox(
-            //       value: isChecked,
-            //       onChanged: (bool? value) {
-            //         setState(() {
-            //           isChecked = value!;
-            //         });
-            //       },
-            //     ),
-            //     Text('Friday'),
-            //   ],
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Checkbox(
-            //       value: isChecked,
-            //       onChanged: (bool? value) {
-            //         setState(() {
-            //           isChecked = value!;
-            //         });
-            //       },
-            //     ),
-            //     Text('Saturday'),
-            //   ],
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Checkbox(
-            //       value: isChecked,
-            //       onChanged: (bool? value) {
-            //         setState(() {
-            //           isChecked = value!;
-            //         });
-            //       },
-            //     ),
-            //     Text('Sunday'),
-            //   ],
-            // ),
-
-            SizedBox(height: 10),
-            Text(
-              'Work Hour',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            DropDownWorkHourList(),
-            SaveBtn()
-          ],
-        ),
-      ),
-    );
-  }
+  State<AddDoctorInformation> createState() => _AddDoctorInfoState();
 }
 
-class DropDownSelectSpecialist extends StatefulWidget {
-  @override
-  DropDownSpecialist createState() => DropDownSpecialist();
-}
+class _AddDoctorInfoState extends State<AddDoctorInformation> {
+  String selectedSpecialist = 'Pediatric';
+  String selectedTime = '08.00 - 11.00';
 
-class DropDownSpecialist extends State<DropDownSelectSpecialist> {
-  String _selectedItem = 'Pediatric'; // Initial selected item
-
-  // List of dropdown items
-  List<String> _dropdownItems = [
+  final List<String> specialistItems = [
     'Pediatric',
     'Dentist',
     'General Practitioner',
@@ -157,95 +24,173 @@ class DropDownSpecialist extends State<DropDownSelectSpecialist> {
     'Surgeon'
   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Specialist'),
-      ),
-      body: Center(
-        child: DropdownButton<String>(
-          value: _selectedItem,
-          icon: Icon(Icons.arrow_downward),
-          iconSize: 24,
-          elevation: 16,
-          style: TextStyle(color: Colors.deepPurple),
-          underline: Container(
-            height: 2,
-            color: Colors.deepPurpleAccent,
-          ),
-          onChanged: (String? newValue) {
-            setState(() {
-              _selectedItem = newValue!;
-            });
-          },
-          items: _dropdownItems.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-        ),
-      ),
-    );
-  }
-}
-
-class DropDownWorkHourList extends StatefulWidget {
-  @override
-  DropDownWorkHour createState() => DropDownWorkHour();
-}
-
-class DropDownWorkHour extends State<DropDownWorkHourList> {
-  String _selectedItem = '08.00 - 11.00'; // Initial selected item
-
-  // List of dropdown items
-  List<String> _dropdownItems = [
+  List<String> timeItems = [
     '08.00 - 11.00',
     '13.00 - 16.00',
     '18.00 - 21.00',
   ];
 
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Specialist'),
-      ),
-      body: Center(
-        child: DropdownButton<String>(
-          value: _selectedItem,
-          icon: Icon(Icons.arrow_downward),
-          iconSize: 24,
-          elevation: 16,
-          style: TextStyle(color: Colors.deepPurple),
-          underline: Container(
-            height: 2,
-            color: Colors.deepPurpleAccent,
-          ),
-          onChanged: (String? newValue) {
-            setState(() {
-              _selectedItem = newValue!;
-            });
-          },
-          items: _dropdownItems.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 20),
+            Text('Select Specialist',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            DropdownButton<String>(
+              isExpanded: true,
+              value: selectedSpecialist,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedSpecialist = newValue!;
+                });
+              },
+              items:
+                  specialistItems.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 10),
+            Text('Select Work Days',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text('Monday'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text('Tuesday'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text('Wednesday'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text('Thursday'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text('Friday'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text('Saturday'),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                    ),
+                    Text('Sunday'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text('Select Work Time',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            DropdownButton<String>(
+              isExpanded: true,
+              value: selectedTime,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedTime = newValue!;
+                });
+              },
+              items: timeItems.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () => print('Add Doctor Information Completed'),
+              child: const Text('Save Information'),
+            ),
+          ],
         ),
       ),
-    );
-  }
-}
-
-class SaveBtn extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text('Save'),
     );
   }
 }
