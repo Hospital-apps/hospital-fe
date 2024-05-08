f6af7a5608855691abce81c321aa07c58eec6ede
Maria Soffie <mariasoffie.17@gmail.com>
2024-05-08 21:03:08 +0800

    update ambulance screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Hospital App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/screens/Patients/AmbulanceScreen.dart
+++ lib/screens/Patients/AmbulanceScreen.dart
@@ -4,9 +4,14 @@ class AmbulanceScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Ambulance'),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Text('Meeting Link: ')],
+        children: [
+          Text('Hospital Phone Number: 15895916522'),
+        ],
       ),
     );
   }
